ec4a62d6794045e350cf023688737ae1107f593c
Sascha Majewsky <sascha.majewsky@pm.me>
2019-07-08 19:20:31 +0200

    added functionality to get /hubs and receive hubs from DB

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 const express = require('express'); //CommonJS Modules
 
+const db = require('./data/hubs-model.js');
+
 const server = express();
 
 server.get('/', (req, res) => {
   res.send('Hello WEB20 node edition');
+});
+
+server.get('/hubs', (req, res) => {
+  db.find()
+    .then(hubs => {
+      res.status(200).json(hubs);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
 });
 
 const port = 5000;
