40123c28a54fde8a4f9c09d52762b4c0bda5fbeb
Sascha Majewsky <sascha.majewsky@pm.me>
2019-07-08 19:48:55 +0200

    added delete functionalities for HTTP requests

--- index.js
+++ index.js
@@ -1,20 +1,54 @@
 const express = require('express'); //CommonJS Modules
 
-const db = require('./data/hubs-model.js');
+const Hubs = require('./data/hubs-model.js');
 
 const server = express();
+
+server.use(express.json()); // Server speaks JSON now
 
 server.get('/', (req, res) => {
   res.send('Hello WEB20 node edition');
 });
 
+// READ IN CRUD
 server.get('/hubs', (req, res) => {
-  db.find()
+  Hubs.find()
     .then(hubs => {
       res.status(200).json(hubs);
     })
     .catch(error => {
       res.status(500).json(error);
+    });
+});
+
+// CREATE IN CRUD
+server.post('/hubs', (req, res) => {
+  // axios.post(url, data) data is in req.body
+  const hubInfo = req.body;
+  Hubs.add(hubInfo)
+    .then(hub => {
+      res.status(201).json(hub);
+    })
+    .catch(error => {
+      res.status(400).json(error);
+    });
+});
+
+// DELETE IN CRUD
+server.delete('/hubs/:id', (req, res) => {
+  const { id } = req.params;
+  Hubs.remove(id)
+    .then(deletedHub => {
+      if (deletedHub) {
+        /* res.status(204).send();   */
+        res.status(204).end();
+      } else {
+        res.status(404).json({ message: 'Hub not found.' });
+      }
+      res.status(201).json(deletedHub);
+    })
+    .catch(error => {
+      res.status(400).json(error);
     });
 });
 
